9ce3bfa03eee174e7379ff38357ec02adb8402c2
owen <owen.chosen@gmail.com>
2023-07-26 18:09:44 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (location) {
+    let distanceInBlocks;
+    if (location > 42) {
+        distanceInBlocks = location - 42;
+    }
+    else {
+        distanceInBlocks = 42 - location;
+    }
+    return distanceInBlocks;
+}
+
+function distanceFromHqInFeet (location) {
+    let distanceInFeet;
+    if (location > 42) {
+        distanceInFeet = (location - 42) * 264;
+    }
+    else {
+        distanceInFeet = (42 - location) * 264;
+    }
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, finish) {
+    let distanceCoveredInFeet;
+    if (start < finish) {
+        distanceCoveredInFeet = (finish - start) * 264;
+    }
+    else {
+        distanceCoveredInFeet = (start - finish) * 264;
+    }
+    return distanceCoveredInFeet;
+}
+
+function calculatesFarePrice (start, finish) {
+    let distanceInFeet = distanceTravelledInFeet (start, finish);
+    if (distanceInFeet <= 400) {
+        return 0;
+    }
+    if (400 < distanceInFeet && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    }
+    if (2000 < distanceInFeet && distanceInFeet < 2500) {
+        return 25;
+    }
+    if (distanceInFeet > 2500) {
+        return "cannot travel that far";
+    }
+}
